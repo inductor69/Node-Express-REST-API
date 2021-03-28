7451643afe0b8c81bdf0c6c2f96521bf372315c1
Hannibal404 <rvrahar@gmail.com>
2021-03-28 17:18:38 +0530

    Added frontend for new user registration

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var HTTP_PORT = 8000
 
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
 app.get("/api/users", (req, res, next) => {
@@ -19,14 +19,14 @@ app.get("/api/users", (req, res, next) => {
     var params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
-            "message":"success",
-            "data":rows
+            "message": "success",
+            "data": rows
         })
-      });
+    });
 });
 
 
@@ -35,45 +35,49 @@ app.get("/api/user/:id", (req, res, next) => {
     var params = [req.params.id]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
-          return;
+            res.status(400).json({ "error": err.message });
+            return;
         }
         res.json({
-            "message":"success",
-            "data":row
+            "message": "success",
+            "data": row
         })
-      });
+    });
+});
+
+app.get("/api/newuser", (req, res, next) => {
+    res.sendFile(__dirname + '/newUser.html');
 });
 
 
 app.post("/api/user/", (req, res, next) => {
-    var errors=[]
-    if (!req.body.password){
+    var errors = []
+    if (!req.body.password) {
         errors.push("No password specified");
     }
-    if (!req.body.email){
+    if (!req.body.email) {
         errors.push("No email specified");
     }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
         return;
     }
     var data = {
         name: req.body.name,
         email: req.body.email,
-        password : md5(req.body.password)
+        password: md5(req.body.password)
     }
-    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
-    var params =[data.name, data.email, data.password]
-    db.run(sql, params, function (err, result) {
-        if (err){
-            res.status(400).json({"error": err.message})
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
             return;
         }
         res.json({
             "message": "success",
             "data": data,
-            "id" : this.lastID
+            "id": this.lastID
         })
     });
 })
@@ -84,25 +88,24 @@ app.patch("/api/user/:id", (req, res, next) => {
     var data = {
         name: req.body.name,
         email: req.body.email,
-        password : req.body.password ? md5(req.body.password) : undefined
+        password: req.body.password ? md5(req.body.password) : undefined
     }
     db.run(
         `UPDATE user set 
            name = coalesce(?,name), 
            email = COALESCE(?,email), 
            password = coalesce(?,password) 
-           WHERE id = ?`,
-        [data.name, data.email, data.password, req.params.id],
+           WHERE id = ?`, [data.name, data.email, data.password, req.params.id],
         (err, result) => {
-            if (err){
-                res.status(400).json({"error": res.message})
+            if (err) {
+                res.status(400).json({ "error": res.message })
                 return;
             }
             res.json({
                 message: "success",
                 data: data
             })
-    });
+        });
 })
 
 
@@ -110,18 +113,17 @@ app.delete("/api/user/:id", (req, res, next) => {
     db.run(
         'DELETE FROM user WHERE id = ?',
         req.params.id,
-        function (err, result) {
-            if (err){
-                res.status(400).json({"error": res.message})
+        function(err, result) {
+            if (err) {
+                res.status(400).json({ "error": res.message })
                 return;
             }
-            res.json({"message":"deleted", rows: this.changes})
-    });
+            res.json({ "message": "deleted", rows: this.changes })
+        });
 })
 
 
 // Root path
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    res.json({ "message": "Ok" })
 });
-
