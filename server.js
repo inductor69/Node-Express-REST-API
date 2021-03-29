45dd30ffd2c74e68dbfc4872f8136a7025cb8245
Hannibal404 <rvrahar@gmail.com>
2021-03-30 02:18:16 +0530

    Tabulated all users data

--- server.js
+++ server.js
@@ -15,24 +15,24 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-app.use(function (req, res, next) {
-  /*var err = new Error('Not Found');
-   err.status = 404;
-   next(err);*/
+app.use(function(req, res, next) {
+    /*var err = new Error('Not Found');
+     err.status = 404;
+     next(err);*/
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-  // Pass to next layer of middleware
-  next();
+    // Pass to next layer of middleware
+    next();
 });
 
 app.get("/api/users", (req, res, next) => {
@@ -43,10 +43,33 @@ app.get("/api/users", (req, res, next) => {
             res.status(400).json({ "error": err.message });
             return;
         }
-        res.json({
-            "message": "success",
-            "data": rows
-        })
+        res.write(`<head>
+        <title>All Users</title>
+        <link rel="stylesheet" href="${__dirname}/css/users.css">
+        <style>
+        table{background-color: #343a40;color:#ced4da;}
+        body{background-color:#212529;}
+        h1{color:#dee2e6;}
+        </style></head>`)
+        var n = rows.length
+        res.write(`
+            <body>
+            <h1>All Users</h1>
+            <table>
+            <tr>
+            <th>Name</th>
+            <th>Email ID</th>
+            <th>Password</th>
+            </tr>`);
+        for (var i = 0; i < n; i++) {
+            data = rows[i];
+            res.write(`<tr>
+                <td>${data.name}</td>
+                <td>${data.email}</td>
+                <td>${data.password}</td>
+                </tr>`);
+        }
+        res.write(`</table></body>`);
     });
 });
 
@@ -141,7 +164,7 @@ app.patch("/api/user/:id", (req, res, next) => {
                 data: data
             })
         });
-        (JSON.stringify(data));
+    (JSON.stringify(data));
 
 })
 
@@ -157,7 +180,7 @@ app.delete("/api/user/:id", (req, res, next) => {
             }
             res.json({ "message": "deleted", rows: this.changes })
         });
-        (JSON.stringify(data));
+    (JSON.stringify(data));
 
 })
 
@@ -165,6 +188,6 @@ app.delete("/api/user/:id", (req, res, next) => {
 // Root path
 app.get("/", (req, res, next) => {
     res.json({ "message": "Ok" })
-    console.log(JSON.stringify({"message": "Ok" }))
-    
+    console.log(JSON.stringify({ "message": "Ok" }))
+
 });
